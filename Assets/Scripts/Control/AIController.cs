5dfc22a66b066e4c00cb634a745417868e56b0cc
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2024-11-27 07:00:14 -0600

    Level and boss music / Final design

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,4 +1,5 @@
 using EndlessRunner.Abilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace EndlessRunner.Control
@@ -6,12 +7,18 @@ namespace EndlessRunner.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float abilityStartTime = 5;
+        [SerializeField] AudioSource levelAudioSource;
         AbilityStore abilityStore;
         float currentTime;
 
         void Awake()
         {
             abilityStore = GetComponent<AbilityStore>();
+        }
+
+        void Start()
+        {
+            levelAudioSource.Stop();
         }
 
         void Update()
